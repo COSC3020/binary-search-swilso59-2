bff20b5c84aa32a026ba12b241f903c739937dcf
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-08-29 10:19:05 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function binarySearch(list, element) {
+    if (list.length < 1) {
+        return -1;
+    }
+    
+    let left = 0;
+    let rigt = list.length -1;
+
+    while (left <= right) {
+        middle = Math.floor((left + right) / 2);
+        if (list[middle] === element) {
+            while (middle > 0 && list[middle - 1] === element) {
+                middle--;
+            }
+            return middle;
+        }
+        else if (list[middle] < element) {
+            left = middle + 1;
+        }
+        else {
+            right = middle - 1;
+        }
+    }
     return -1;
 }
