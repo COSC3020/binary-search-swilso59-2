d35efddba49753bb65c3857592c37bcc80d34bea
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-09-05 11:59:08 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
 function binarySearch(list, element) {
+    // If list is empty return -1
     if (list.length < 1) {
         return -1;
     }
     
     let left = 0;
     let right = list.length -1;
-
+    
     while (left <= right) {
+        
         middle = Math.floor((left + right) / 2);
         if (list[middle] === element) {
             while (middle > 0 && list[middle - 1] === element) {
